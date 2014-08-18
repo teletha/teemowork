fda9ee0d0af362b8b933ec26984f13e2508128d0
Teletha <Teletha.T@gmail.com>
2014-08-18 15:07:51 +0900

    Isolate from booton.

---
+++ src/project/java/Project.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (C) 2014 Teemowork Development Team
+ * 
+ * Licensed under the BSD License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *          http://opensource.org/licenses/BSD-2-Clause
+ */
+public class Project extends bee.api.Project {
+
+    {
+        product("teemowork", "Teemowork", "1.0");
+
+        require("npc", "booton", "0.1");
+    }
+}
