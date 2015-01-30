0ca23a6f1fadd0236639741a3a84bc6a7d265ea7
Teletha <Teletha.T@gmail.com>
2015-01-30 16:32:55 +0900

    Progress...

---
+++ src/main/java/teemowork/ChampionSelectWidget.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2015 Nameless Production Committee
+ *
+ * Licensed under the MIT License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://opensource.org/licenses/mit-license.php
+ */
+package teemowork;
+
+import jsx.ui.VirtualStructure;
+import jsx.ui.Widget;
+
+/**
+ * @version 2015/01/30 14:27:54
+ */
+public class ChampionSelectWidget extends Widget {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void virtualize(VirtualStructure $〡) {
+    }
+}
